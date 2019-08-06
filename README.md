486ad0eabaa5003a32a5c6db917d80a073a74dbb
postolenko <denis.postolenko@gmail.com>
2019-08-06 17:58:22 +0300

    GS

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 # http://postolenko.github.io/gs/news_page.html
 # http://postolenko.github.io/gs/card.html
 # http://postolenko.github.io/gs/card_2.html
+# http://postolenko.github.io/gs/payment_details.html
+# http://postolenko.github.io/gs/thanks.html
--- js/scripts_2.js
+++ js/scripts_2.js
@@ -4,10 +4,69 @@ $(window).on("load",function(){
 
 $(document).ready(function() {
 
+	$(".input_placeholder").each(function() {
+		$(this).find("input, textarea").attr("placeholder", "");
+	});
+
 	$(".close_good").on("click", function(e) {
 		e.preventDefault();
 		parentBlock = $(this).closest(".card_good_box");
 		parentBlock.remove();
 	});
-	
+
+	$(".count_box button").click(function(e) {
+        e.preventDefault();
+        parentBlock = $(this).closest(".count_box");
+        cardRow = parentBlock.closest(".table_row");
+        priceValEl = cardRow.find(".price_val");
+        // priceVal = parseFloat( priceValEl.text() );
+        priceVal = parseFloat(priceValEl.text().replace(',', '.'));
+        countInput = parentBlock.find(".count_num input");
+        countVal = countInput.val();
+        if(countVal == "") {
+            countVal = 1;
+        }
+        if( $(this).hasClass("minus_btn") && countVal > 1 ) {
+            countVal--;
+        } else if( $(this).hasClass("plus_btn")) {
+            countVal++;
+        }
+        countInput.val(countVal);
+        priceVal = priceVal.toFixed(2);
+        cardRow.find(".price_total_val").text(priceVal * countVal);
+    });
+
+    $(".count_num input").on("keyup", function(e) {
+    	cardRow = $(this).closest(".table_row");
+    	countVal = $(this).val();
+    	priceValEl = cardRow.find(".price_val");
+        priceVal = parseInt( priceValEl.text() );
+        cardRow.find(".price_total_val").text(priceVal * countVal);
+    });
+
+    $(".del_btn_2").on("click", function(e) {
+		e.preventDefault();
+		parentBlock = $(this).closest(".table_row");
+		parentBlock.remove();
+	});
+
+	$(".input_placeholder").on("click", function(e) {
+		e.preventDefault();
+		$(".input_placeholder").each(function() {
+			if($(this).find("input, textarea").val() == "") {
+				$(this).removeClass("active");
+			}
+		});		
+		$(this).addClass("active");
+		$(this).find("input, textarea").focus();
+	});
+
+	$(document).mouseup(function (e){
+        $(".input_placeholder").each(function() {
+			if($(this).find("input, textarea").val() == "") {
+				$(this).removeClass("active");
+			}
+		});
+    });
+
 });
